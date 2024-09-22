16e4bd2c7dd3896d390f5a1753d897f47f60708f
m2x07 <meet2x07@gmail.com>
2024-09-22 07:24:47 +0530

    Re-factor themes as recommeded by docs and switch to tokyonight

--- .config/wezterm/themes.lua
+++ .config/wezterm/themes.lua
@@ -1,40 +1,7 @@
 local M
 
 M = {
-	["m2x07-black"] = {
-		foreground = "#ffffff",
-		background = "#040811",
-
-		cursor_fg = "#000000",
-		cursor_bg = "#ffffff",
-		cursor_border = "#ffffff",
-
-		selection_bg = "#1A53AC",
-		selection_fg = "#E6EDF3",
-
-		split = "#333333",
-
-		ansi = {
-			"#000000",
-			"#fc5555",
-			"#5be599",
-			"#fcfc14",
-			"#0000ff",
-			"#fc14ec",
-			"#14f4fc",
-			"#ffffff",
-		},
-		brights = {
-			"#555555",
-			"#fc5555",
-			"#5be599",
-			"#fcfc14",
-			"#0000ff",
-			"#fc14ec",
-			"#14f4fc",
-			"#ffffff",
-		},
-	},
+	
 }
 
 return M
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,11 +1,10 @@
 local wez = require("wezterm")
 local config = wez.config_builder()
-local themes = require("themes")
-local keybinds = require('keybinds')
-config.color_schemes = themes
+local keybinds = require("keybinds")
 config.keys = keybinds
 
-config.color_scheme = "m2x07-black"
+config.color_scheme = "tokyonight_night"
+config.color_scheme_dirs = { "~/.config/wezterm/colors" }
 -- config.color_scheme = 'Abernathy'
 -- config.color_scheme = 'Aci (Gogh)'
 -- config.color_scheme = 'Argonaut'
@@ -13,15 +12,15 @@ config.color_scheme = "m2x07-black"
 config.font = wez.font("JetBrainsMonoNL Nerd Font", { weight = "Regular" })
 config.font_size = 10
 config.freetype_interpreter_version = 35
-config.freetype_load_target = 'Light'
-config.freetype_render_target = 'HorizontalLcd'
+config.freetype_load_target = "Light"
+config.freetype_render_target = "HorizontalLcd"
 
 config.animation_fps = 60
 config.scrollback_lines = 5000
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.85
-config.window_decorations = "NONE"
+config.window_background_opacity = 1
+-- config.window_decorations = "NONE"
 config.initial_rows = 40
 
 -- NOTE: use when having issues when finding name of the key for keybinds
@@ -39,8 +38,8 @@ config.inactive_pane_hsb = {
 	saturation = 1,
 }
 config.leader = {
-    key = 'a',
-    mods = 'CTRL',
-    timeout_milliseconds = 1500,
+	key = "a",
+	mods = "CTRL",
+	timeout_milliseconds = 1500,
 }
 return config
