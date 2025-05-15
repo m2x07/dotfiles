053d7550cb7d77fcb35b7c56c8d2d05e3699a9f9
m2x07 <meet2x07@gmail.com>
2025-05-15 18:25:25 +0530

    update hyprland config

--- .config/hypr/wallrefresh.sh
+++ .config/hypr/wallrefresh.sh
@@ -2,7 +2,10 @@
 
 WALLPAPER_DIR="$HOME/.local/share/backgrounds/"
 CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+# Get the name of the focused monitor with hyprctl
+FOCUSED_MONITOR=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
+# Get a random wallpaper that is not the current one
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
-NEW_WALL=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
-
-hyprctl hyprpaper reload eDP-1,"$NEW_WALL"
+# Apply the selected wallpaper
+hyprctl hyprpaper reload "$FOCUSED_MONITOR","$WALLPAPER"
