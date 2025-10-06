9ff271c8ce1184b277a4f72fbd306e4880d2286b
m2x07 <meet2x07@gmail.com>
2025-10-06 21:40:16 +0530

    update battery_notify.sh logic to not send notifications more than once when suitable

--- .local/bin/battery_notify.sh
+++ .local/bin/battery_notify.sh
@@ -3,11 +3,32 @@
 BAT_PATH="/sys/class/power_supply/BAT0"
 CAPACITY=$(cat "$BAT_PATH/capacity")
 STATUS=$(cat "$BAT_PATH/status")
+FLAGFILE=/tmp/batterynotificationsent
 
-if [ "$STATUS" == "Discharging" ] && [ "$CAPACITY" -le 20 ]; then
-    notify-send -a "battery_notify.sh" -u normal "Low battery warning!" "Only ${CAPACITY}% remaining"
-elif [ "$STATUS" == "Discharging" ] && [ "$CAPACITY" -le 10 ]; then
-    notify-send -a "battery_notify.sh" -u critical "Battery level critical: ${CAPACITY}%" "Plug in a power source now"
-elif [ "$STATUS" == "Charging" ] && [ "$CAPACITY" -gt 85 ]; then
-    notify-send -a "battery_notify.sh" -u normal "Battery fully charged" "You may now unplug the charger"
+if [ "$STATUS" == "Discharging" ]; then
+    if [ "$CAPACITY" -gt 20 ]; then
+        if [ -e $FLAGFILE ]; then
+            rm $FLAGFILE
+        fi
+    elif [ "$CAPACITY" -le 10 ]; then
+        notify-send -a "battery_notify.sh" -u critical "Battery level critical: ${CAPACITY}%" "Plug in a power source now"
+    elif [ "$CAPACITY" -le 20 ]; then
+        if [ ! -e $FLAGFILE ]; then
+            echo 5
+            notify-send -a "battery_notify.sh" -u normal "Low battery warning!" "Only ${CAPACITY}% remaining"
+            touch $FLAGFILE
+        fi
+    fi
+else
+    if [ "$CAPACITY" -ge 85 ]; then
+        if [ ! -e $FLAGFILE ]; then
+            echo 3
+            notify-send -a "battery_notify.sh" -u normal "Battery fully charged" "You may now unplug the charger"
+            touch $FLAGFILE
+        fi
+    else
+        if [ -e $FLAGFILE ]; then
+            rm $FLAGFILE
+        fi
+    fi
 fi
